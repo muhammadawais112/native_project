474461c90c4dd2a2e9e6018fe0a3419e64ee86b7
muhammadawais112 <simpleboy1917@gmail.com>
2023-03-18 22:39:03 +0500

    font family change

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text  style={{fontSize:40,fontFamily:"Sono-Regular"}} >App</Text>
+    </View>
+  )
+}
